a595b1ee216922037af59b3c42475c5ced151147
Bernhard Mallinger <bernhard.mallinger@eox.at>
2020-04-21 10:54:38 +0200

    Distinguish between dev/prod in prepare()
    Prod is default, dev can be enabled with `dev=True`.

--- jupyter-user/edc.py
+++ jupyter-user/edc.py
@@ -5,13 +5,18 @@ from IPython.display import display, HTML, YouTubeVideo, Markdown
 from dotenv.main import find_dotenv, DotEnv
 
 
-# TODO: configure dev or production!!!!
-ENTITY_METADATA_URL = "https://api.dev.hub.eox.at/metadata"
-CONTRIBUTIONS_API_URL = "https://contributions-api.dev.hub.eox.at"
+ENTITY_METADATA_URL = {
+    "dev": "https://api.dev.hub.eox.at/metadata",
+    "prod": "https://api.hub.eox.at/metadata",
+}
+CONTRIBUTIONS_API_URL = {
+    "dev": "https://contributions-api.dev.hub.eox.at",
+    "prod": "https://contributions-api.hub.eox.at",
+}
 
 
-def _get_requirement_name_mapping():
-    response = requests.get(ENTITY_METADATA_URL)
+def _get_requirement_name_mapping(dev: bool):
+    response = requests.get(ENTITY_METADATA_URL["dev" if dev else "prod"])
     response.raise_for_status()
     return {
         data['entity']['id']: data['entity']['name']
@@ -19,21 +24,22 @@ def _get_requirement_name_mapping():
     }
 
 
-def _get_notebook(notebook_id: str):
-    response = requests.get(f"{CONTRIBUTIONS_API_URL}/notebooks/{notebook_id}")
+def _get_notebook(notebook_id: str, dev: bool):
+    response = requests.get(f"{CONTRIBUTIONS_API_URL['dev' if dev else 'prod']}/notebooks/{notebook_id}")
     response.raise_for_status()
     return response.json()
 
 
-def prepare(notebook_id: str):
+def prepare(notebook_id: str, dev: bool = False):
+    # Legacy. This will be removed in an upcoming release.
 
     # actual setup
     dot_env = DotEnv(find_dotenv())
     dot_env.set_as_environment_variables()
 
     # the rest displays information
-    requirement_name_mapping = _get_requirement_name_mapping()
-    notebook = _get_notebook(notebook_id)
+    requirement_name_mapping = _get_requirement_name_mapping(dev=dev)
+    notebook = _get_notebook(notebook_id, dev=dev)
 
     requirements = [
         requirement_name_mapping.get(req, req)
