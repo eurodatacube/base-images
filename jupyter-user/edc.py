7f1fd3af5c13490f02cb42fc1ad20ed51a75c511
Bernhard Mallinger <bernhard.mallinger@eox.at>
2020-05-20 12:14:11 +0200

    Add compatiblity check to edc.py

--- jupyter-user/edc.py
+++ jupyter-user/edc.py
@@ -1,8 +1,13 @@
+import collections
+import difflib
+import os
 import os.path
+import re
 import requests
 from textwrap import dedent
+from typing import List, Tuple
 
-from IPython.display import display, HTML, YouTubeVideo, Markdown
+from IPython.display import display, Markdown
 from dotenv.main import find_dotenv, DotEnv
 
 
@@ -19,14 +24,13 @@ CONTRIBUTIONS_API_URL = {
 def _get_requirement_name_mapping(dev: bool):
     response = requests.get(ENTITY_METADATA_URL["dev" if dev else "prod"])
     response.raise_for_status()
-    return {
-        data['entity']['id']: data['entity']['name']
-        for data in response.json()
-    }
+    return {data["entity"]["id"]: data["entity"]["name"] for data in response.json()}
 
 
 def _get_notebook(notebook_id: str, dev: bool):
-    response = requests.get(f"{CONTRIBUTIONS_API_URL['dev' if dev else 'prod']}/notebooks/{notebook_id}")
+    response = requests.get(
+        f"{CONTRIBUTIONS_API_URL['dev' if dev else 'prod']}/notebooks/{notebook_id}"
+    )
     response.raise_for_status()
     return response.json()
 
@@ -47,7 +51,8 @@ def prepare(notebook_id: str, dev: bool = False):
         for req in notebook.get("requirements", [])
     ]
 
-    info = dedent(f"""
+    info = dedent(
+        f"""
         ***Notebook Title***  
         {notebook['name']}
         
@@ -58,7 +63,8 @@ def prepare(notebook_id: str, dev: bool = False):
     )
 
     if requirements:
-        info += dedent(f"""
+        info += dedent(
+            """
             ***Notebook Dependencies***  
             This notebook requires an active subscription to:
             """
@@ -90,7 +96,8 @@ def print_info(notebook_id: str, dev: bool = False):
         for req in notebook.get("requirements", [])
     ]
 
-    info = dedent(f"""
+    info = dedent(
+        f"""
         ***Notebook Title***  
         {notebook['name']}
         
@@ -101,7 +108,8 @@ def print_info(notebook_id: str, dev: bool = False):
     )
 
     if requirements:
-        info += dedent(f"""
+        info += dedent(
+            """
             ***Notebook Dependencies***  
             This notebook requires an active subscription to:
             """
@@ -115,11 +123,11 @@ def setup_environment_variables():
     """Called in every notebook to inject credentials to environment"""
     dot_env = DotEnv(find_dotenv())
     dot_env.set_as_environment_variables()
-    info =  (
-        "API credentials have automatically been injected for your active subscriptions.  \n" +
-        "The following environment variables are now available:\n" +
-        "".join(f"* `{k}`\n" for k in dot_env.dict().keys()) +
-        "\n"
+    info = (
+        "API credentials have automatically been injected for your active subscriptions.  \n"
+        + "The following environment variables are now available:\n"
+        + "".join(f"* `{k}`\n" for k in dot_env.dict().keys())
+        + "\n"
     )
 
     user_dot_env_path = "~/custom.env"
@@ -128,10 +136,118 @@ def setup_environment_variables():
     user_vars = user_dot_env.dict()
     if user_vars:
         info += (
-            f"The following additional environment variables have been loaded from `{user_dot_env_path}`:\n" +
-            "".join(f"* `{k}`\n" for k in user_vars)
+            f"The following additional environment variables have been loaded from `{user_dot_env_path}`:\n"
+            + "".join(f"* `{k}`\n" for k in user_vars)
         )
 
     info += "------\n"
 
     display(Markdown(info))
+
+
+release_url = "https://contributions-api.dev.hub.eox.at/base-image-release-notes/{tag}"
+
+# Example: -ipython                   7.13.0           py37hc8dfbb8_2    conda-forge
+conda_list_output_regex = (
+    r"^[-+](?P<name>\S+)\s+(?P<major_version>\d+)\.\S+\s+\S+\s+\S+$"
+)
+# TODO: use semver for anything more complicated
+
+
+def _get_release_message(tag: str) -> str:
+    response = requests.get(release_url.format(tag=tag))
+    response.raise_for_status()
+    return response.text
+
+
+def _calculate_diff_lines(old: str, new: str) -> List[str]:
+    diff = difflib.unified_diff(old.splitlines(), new.splitlines(), n=0, lineterm="")
+    # remove position markers ('@@ -25,2 +25,2 @@')
+    diff = [
+        entry for entry in diff if not (entry.startswith("@@") and entry.endswith("@@"))
+    ]
+    # remove "header" (---, +++)
+    diff = diff[2:]
+    return diff
+
+
+def _group_major_minor_added_remove(
+    diff_lines: List[str],
+) -> Tuple[List[str], List[str], List[str], List[str]]:
+    """Takes diff lines and returns them grouped by meaning of change"""
+    libraries = collections.defaultdict(list)
+
+    for line in diff_lines:
+        match = re.match(conda_list_output_regex, line)
+        if match:
+            libraries[match.group("name")].append(
+                {"major_version": match.group("major_version"), "line": line}
+            )
+
+    major, minor, added, removed = [], [], [], []
+    for lib, versions in libraries.items():
+        if len(versions) == 2:
+            if versions[0]["major_version"] != versions[1]["major_version"]:
+                major.extend((versions[0]["line"], versions[1]["line"]))
+            else:
+                minor.extend((versions[0]["line"], versions[1]["line"]))
+
+        elif len(versions) == 1:
+            sign = versions[0]["line"][0]
+            if sign == "-":
+                removed.append(versions[0]["line"])
+            elif sign == "+":
+                added.append(versions[0]["line"])
+
+    return major, minor, added, removed
+
+
+def _append_nl(seq: List[str]) -> List[str]:
+    return [f"{i}\n" for i in seq]
+
+
+def check_compatibility(tag: str) -> None:
+    current_image_tag = "v" + os.environ["JUPYTER_IMAGE"].split(":", 2)[1]
+    if current_image_tag == tag:
+        print("Notebook is compatible")
+    else:
+        msg = dedent(
+            f"""
+            ## Base image difference detected
+            This notebook has been verified using the [base image **{tag}**](https://github.com/eurodatacube/base-images/releases/tag/{tag}),
+            whereas you are currently running [base image **{current_image_tag}**](https://github.com/eurodatacube/base-images/releases/tag/{current_image_tag}).
+
+            If you experience any problems, please consult the [marketplace](https://eurodatacube.com/marketplace) for a more recent version of the notebook.
+
+            
+
+            These changes occurred in base image in between these versions:
+
+            """
+        )
+        notebook_image_release_msg = _get_release_message(tag)
+        current_image_release_msg = _get_release_message(current_image_tag)
+
+        diff_lines = _calculate_diff_lines(
+            old=notebook_image_release_msg, new=current_image_release_msg,
+        )
+
+        major, minor, added, removed = _group_major_minor_added_remove(diff_lines)
+
+        if added:
+            msg += f"### New libraries:\n```\n{''.join(_append_nl(added))}```\n"
+
+        if removed:
+            print("### Removed libraries:")
+            print("\n".join(removed))
+            print()
+
+        if major:
+            print("### Libraries with major version differences:")
+            print("\n".join(major))
+            print()
+
+        if minor:
+            msg += f"### Libraries with minor version differences:\n```\n{''.join(_append_nl(minor))}```\n"
+
+        display(Markdown(msg))
