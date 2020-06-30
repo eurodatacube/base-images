706e31260974badae651f835b15920c13e0761d2
Bernhard Mallinger <bernhard.mallinger@eox.at>
2020-06-30 14:37:38 +0200

    Install edc from base image to new kernels

--- install_kernel.sh
+++ install_kernel.sh
@@ -4,6 +4,9 @@ set -eux -o pipefail
 
 DEST_PATH=$1
 
-conda env create --file base-environment.yaml --prefix $DEST_PATH
+conda env create --file base-environment.yaml --prefix "$DEST_PATH"
+
+# install edc from base image (TODO: make it into package, installable from git)
+cp -r /opt/conda/lib/python3.7/site-packages/edc "${DEST_PATH}/lib/python*/site-packages"
 
 conda clean --all -f -y
